871d7ac292ab2ffda69fe824a36aaf29a6370669
Jose Luis  Franconetti Olmedo <joseluis.franconetti@gmail.com>
2015-03-26 12:59:11 +0100

    Fixed problem with the project structure.

---
+++ library/src/main/java/com/mobilejazz/library/BaseObject.java
@@ -0,0 +1,183 @@
+package com.mobilejazz.library;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+
+public class BaseObject implements java.io.Serializable
+{
+	private static final long serialVersionUID = 1L;
+	
+	private String key;
+	private transient Date lastUpdate;
+	private transient boolean hasChanges;
+	private transient ObjectContext context;
+	
+	// ---------------------------------------------------------------------------------------------------------
+	// Getters and Setters
+	// ---------------------------------------------------------------------------------------------------------
+	
+	public String getKey()
+	{
+		return key;
+	}
+	
+	public void setKey(String key)
+	{
+		this.key = key;
+	}
+
+	public Date getLastUpdate()
+	{
+		return lastUpdate;
+	}
+
+	public void setLastUpdate(Date lastUpdate)
+	{
+		this.lastUpdate = lastUpdate;
+	}
+
+	public boolean hasChanges() 
+	{
+		return hasChanges;
+	}
+
+	public void setHasChanges(boolean hasChanges) 
+	{
+		this.hasChanges = hasChanges;
+	}
+	
+	// ---------------------------------------------------------------------------------------------------------
+	// Public Methods
+	// ---------------------------------------------------------------------------------------------------------
+	
+	public void flagChanges()
+	{
+		this.setHasChanges(true);
+	}
+	
+	public String getBaseObjectType()
+	{
+		return this.getClass().getSimpleName();
+	}
+	
+	// ---------------------------------------------------------------------------------------------------------
+	// Creating instances and initializing
+	// ---------------------------------------------------------------------------------------------------------
+
+    public BaseObject(){};
+
+	public BaseObject(String key)  
+	{
+		super();
+		
+		this.key = key;
+		this.hasChanges = (key != null);
+	}
+	
+	// ---------------------------------------------------------------------------------------------------------
+	// Object context management
+	// ---------------------------------------------------------------------------------------------------------
+		
+	public ObjectContext getContext()
+	{
+		return context;
+	}
+	
+	void setContext(ObjectContext context)
+	{
+		this.context = context;
+	}
+	
+	public void deleteObjectFromContext()
+	{
+		if (this.context != null)
+			this.context.deleteObject(this);
+	}
+	
+//	public boolean registerToContext(ObjectContext context)
+//	{
+//		if (context != null)
+//		{
+//			boolean succeed = context.insertObject(this);
+//
+//			if (succeed == false)
+//				this.setContext(null);
+//
+//			return succeed;
+//		}
+//		else
+//		{
+//			this.deleteObjectFromContext();
+//			return true;
+//		}
+//	}
+
+	// ---------------------------------------------------------------------------------------------------------
+	// Serialization
+	// ---------------------------------------------------------------------------------------------------------
+	
+	protected final byte[] serialize()
+	{
+		try 
+		{
+			ByteArrayOutputStream bos = new ByteArrayOutputStream();
+			ObjectOutput out = new ObjectOutputStream(bos);   
+			out.writeObject(this);
+			
+			byte[] bytes = bos.toByteArray();
+			
+			out.close();
+			bos.close();
+			
+			return bytes;
+		} 
+		catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	protected final static BaseObject deserialize(byte[] bytes)
+	{
+		try 
+		{
+			ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+			ObjectInput in = null;
+
+			in = new ObjectInputStream(bis);
+			BaseObject bo = (BaseObject)in.readObject(); 
+			
+			bis.close();
+			in.close();
+			
+			return bo;
+		} 
+		catch (Exception e) 
+		{
+			// Nothing to do, just catch the exception.
+			e.printStackTrace();
+		} 
+		
+		return null;
+	}
+	
+	// ---------------------------------------------------------------------------------------------------------
+	// Typical Methods
+	// ---------------------------------------------------------------------------------------------------------
+	
+	@Override
+	public String toString() 
+	{
+		return "BaseObject [key=" + key + ", lastUpdate=" + lastUpdate
+				+ ", hasChanges=" + hasChanges + "]";
+	}
+}
---
+++ library/src/main/java/com/mobilejazz/library/PersistentSQLiteObject.java
@@ -0,0 +1,60 @@
+package com.mobilejazz.library;
+
+public class PersistentSQLiteObject extends PersistentObject 
+{
+	public static long UNDEFINED_DATABASE_ID = -1;
+	
+	private long dbID;
+	private PersistentSQLiteStore persistentStore;
+
+	// ---------------------------------------------------------------------------------------------------------
+	// Creating instances and initializing
+	// ---------------------------------------------------------------------------------------------------------
+	
+	public PersistentSQLiteObject()
+	{
+		super();
+		this.dbID = UNDEFINED_DATABASE_ID;
+	}
+	
+	public PersistentSQLiteObject(long dbID)
+	{
+		super();
+		this.dbID = dbID;
+		this.setHasChanges(true);
+	}
+
+	// ---------------------------------------------------------------------------------------------------------
+	// Getters & Setters
+	// ---------------------------------------------------------------------------------------------------------
+
+	public long getDbID() 
+	{
+		return dbID;
+	}
+	
+	protected void setDbID(long dbID)
+	{
+		this.setHasChanges(dbID != this.dbID);
+		this.dbID = dbID;
+	}
+
+	protected PersistentSQLiteStore getPersistentStore() 
+	{
+		return persistentStore;
+	}
+
+	protected void setPersistentStore(PersistentSQLiteStore persistentStore) 
+	{
+		this.persistentStore = persistentStore;
+	}
+	
+	@Override
+	protected void setHasChanges(boolean hasChanges)
+	{
+		super.setHasChanges(hasChanges);
+		
+		if (persistentStore != null && hasChanges == true)
+			persistentStore.didChangePersistentSQLiteObject(this);
+	}
+}
